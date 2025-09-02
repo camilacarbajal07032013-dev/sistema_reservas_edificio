a5a45028418451c08c4a8e4e80c58f03741c33ce
DVI.dev <camilacarbajal07032013@gmail.com>
2025-09-02 10:38:54 -0500

    Update settings.py

--- edificio/settings.py
+++ edificio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-clave-temporal-para-desarrollo')
+SECRET_KEY = 'django-secret-key-muy-larga-y-segura-123456789'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
