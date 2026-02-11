38bac916c7607145139aa64e761f653c5be21559
DVI.dev <camilacarbajal07032013@gmail.com>
2026-02-11 15:43:08 -0500

    Update urls.py

--- edificio/urls.py
+++ edificio/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from reservas.views import setup_admin_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reservas.urls')),
+    path('setup-admin-xyz/', setup_admin_user),
 ]
